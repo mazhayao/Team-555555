bdf2525d90395b597806f7eeaf0c0454a29ffb80
Roger <rogers5thadress@gmail.com>
2018-11-08 21:07:35 -0800

    45 vars

--- project1.py
+++ project1.py
@@ -1,8 +1,4 @@
-
-# coding: utf-8
-
-# In[1]:
-
+ # ----------- Overhead -------------
 
 import pandas as pd
 import numpy as np
@@ -10,46 +6,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 # from pandas import ExcelWriter
-import os
-cwd = os.getcwd()
-cwd
-
-
-# In[29]:
-
 
 df = pd.read_csv('NY property csv.csv')
 
 
-# In[30]:
 
+# --------------- Fill in NAs and 0s ----------
 
 # Create new variable BORO
 df['BORO'] = ''
 df['BORO'] = df.BBLE.str[0]
-
-# sns.distplot(df[df['BORO'] == '5']['LTFRONT'],bins = 1000, kde=False)
-# plt.xlim(0,500)
-
-
-# In[31]:
-
 
 # ZIP, sort by BBLE and forward fill
 df = df.sort_values(by = ['BBLE'])
 df['ZIP'].fillna(method='ffill',inplace = True)
 
 
-# In[15]:
-
-
 df['ll_rate'] = df['LTFRONT']/df['LTDEPTH']
-
-
 mean_llrate = np.mean(df['ll_rate'].dropna())
 df['LTFRONT'] = df['LTFRONT'].fillna(value = (df['LTDEPTH'] * mean_llrate))
 df['LTFRONT'] = df['LTDEPTH'].fillna(value = (df['LTFRONT'] / mean_llrate))
-
 np.std(df['LTFRONT'])
 # std = 73.7 without group
 # two methods to set 0 values as missing values
@@ -61,21 +37,11 @@ df['LTFRONT'] = df.groupby(['BORO','TAXCLASS'])['LTFRONT'].transform(lambda x: x
 df['LTFRONT'] = df.groupby(['TAXCLASS'])['LTFRONT'].transform(lambda x: x.fillna(x.mean()))
 
 
-np.isnan(df['LTFRONT']).sum()
-
-
-# In[16]:
-
-
 df.loc[df['LTDEPTH'] == 0, 'LTDEPTH'] = np.nan
 df['LTDEPTH'] = df.groupby(['BORO','TAXCLASS'])['LTDEPTH'].transform(lambda x: x.fillna(x.mean()))
 df['LTDEPTH'] = df.groupby(['TAXCLASS'])['LTDEPTH'].transform(lambda x: x.fillna(x.mean()))
 
-np.isnan(df['LTDEPTH']).sum()
-
-
-# In[181]:
-
+# np.isnan(df['LTDEPTH']).sum()
 
 # Calculate BLDFRONT/BLDDEPTH ratio
 df.loc[df['BLDFRONT'] == 0, 'BLDFRONT'] = np.nan
@@ -91,9 +57,6 @@ df['BLDFRONT'] = df['BLDFRONT'].fillna(value = (df['BLDDEPTH'] * median_bbrate))
 df['BLDDEPTH'] = df['BLDDEPTH'].fillna(value = (df['BLDFRONT'] / median_bbrate))
 
 
-# In[19]:
-
-
 # For BLDFRONT and BLDDEPTH, if group by both BORO and TAXCLASS, there will appear NaN as well.
 df['BLDFRONT'] = df.groupby(['BORO','TAXCLASS'])['BLDFRONT'].transform(lambda x: x.fillna(x.mean()))
 df['BLDFRONT'] = df.groupby(['TAXCLASS'])['BLDFRONT'].transform(lambda x: x.fillna(x.mean()))
@@ -105,32 +68,15 @@ df['BLDDEPTH'] = df.groupby(['TAXCLASS'])['BLDDEPTH'].transform(lambda x: x.fill
 np.isnan(df['BLDFRONT']).sum()
 np.isnan(df['BLDDEPTH']).sum()
 
-
-# In[35]:
-
-
 df['STORIES'] = df.groupby(['ZIP','TAXCLASS'])['STORIES'].transform(lambda x: x.fillna(x.mean()))
 df['STORIES'] = df.groupby(['TAXCLASS'])['STORIES'].transform(lambda x: x.fillna(x.mean()))
 
-np.isnan(df['STORIES']).sum()
-
-
-# In[194]:
-
+# np.isnan(df['STORIES']).sum()
 
 # building volume and bins accordingly
 df['bldvol'] = df['BLDFRONT'] * df['BLDDEPTH'] * df['STORIES']
 
-# xhigh = 60000
-# plt.xlim(0,xhigh)
-# temp = df[df['bldvol'] <= xhigh]
-# sns.distplot(temp['bldvol'],bins=200, kde=False)
-
 df['bldvol_bin'] = pd.qcut(df['bldvol'], 100, labels = False, duplicates = 'drop')
-
-
-# In[200]:
-
 
 # FULLVAL
 df.loc[df['FULLVAL'] == 0, 'FULLVAL'] = np.nan
@@ -140,16 +86,10 @@ df['FULLVAL'] = df.groupby(['BORO','bldvol_bin'])['FULLVAL'].transform(lambda x:
 np.isnan(df['FULLVAL']).sum()
 
 
-# In[198]:
-
-
 # AVLAND
 df.loc[df['AVLAND'] == 0, 'AVLAND'] = np.nan
 df['AVLAND'] = df.groupby(['BORO','bldvol_bin'])['AVLAND'].transform(lambda x: x.fillna(x.mean()))
 np.isnan(df['AVLAND']).sum()
-
-
-# In[199]:
 
 
 # AVTOT
@@ -158,9 +98,30 @@ df['AVTOT'] = df.groupby(['BORO','bldvol_bin'])['AVTOT'].transform(lambda x: x.f
 np.isnan(df['AVTOT']).sum()
 
 
-# In[201]:
-
-
+## Build 3 sizes
+## Build LotArea, Building Area, Building Volume
 df['lotarea'] = df['LTDEPTH'] * df['LTFRONT']
 df['bldarea'] = df['BLDFRONT'] * df['BLDDEPTH']
+df['bldvol'] = df['bldarea'] * df['STORIES']
+
+## Build 9 values
+## FV = FULLVAL, AL = AVLAND, AT = AVTOT
+## LA = LOTAREA, BA = BLDAREA, BV = BLDVOL
+df['fv_la'] = df['FULLVAL'] / df['lotarea']
+df['fv_ba'] = df['FULLVAL'] / df['bldarea']
+df['fv_bv'] = df['FULLVAL'] / df['bldvol']
+df['al_la'] = df['AVLAND'] / df['lotarea']
+df['al_ba'] = df['AVLAND'] / df['bldarea']
+df['al_bv'] = df['AVLAND'] / df['bldvol']
+df['at_la'] = df['AVTOT'] / df['lotarea']
+df['at_ba'] = df['AVTOT'] / df['bldarea']
+df['at_bv'] = df['AVTOT'] / df['bldvol']
+
+## Build 45 Group by values
+ninevars = ['fv_la','fv_ba','fv_bv','al_la','al_ba','al_bv','at_la','at_ba','at_bv']
+df = df.join(df.groupby(['ZIP'])[ninevars].mean(), on='ZIP', rsuffix='_zip')
+df = df.join(df.groupby(['ZIP3'])[ninevars].mean(), on='ZIP3', rsuffix='_zip3')
+df = df.join(df.groupby(['TAXCLASS'])[ninevars].mean(), on='TAXCLASS', rsuffix='_taxclass')
+df = df.join(df.groupby(['BORO'])[ninevars].mean(), on='BORO', rsuffix='_boro')
+
 
